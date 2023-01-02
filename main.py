e0891709922e0209d173b1cb05f8e2273147f8ec
Hongbin Miao <59853402+zslrmhb@users.noreply.github.com>
2023-01-02 12:31:21 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Running the Demo
+
 from asr import ASRService
 from nlp import NLPService
 from tts import TTSService
@@ -9,10 +11,17 @@ tts_service = TTSService()
 audio2face_service = Audio2FaceService()
 
 while True:
+    # speech recognition
     asr_service.run()
     print(asr_service.transcript)
+    
+    # natural language processing with the help of Wikipedia API
     nlp_service.wiki_query(asr_service.transcript)
     output = nlp_service.nlp_query()
     print(output)
+    
+    # text-to-speech
     audio_bytes = tts_service.get_audio_bytes(output)
+    
+    # Audio2Face Animation
     audio2face_service.make_avatar_speaks(audio_bytes)
