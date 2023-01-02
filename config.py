612768724b84728d0ccc1e1f73e0242ce80aa6da
Hongbin Miao <59853402+zslrmhb@users.noreply.github.com>
2023-01-02 12:27:42 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
+# configuration for accessing the remote local host on the Google Cloud Server
 URI = ""  # This will be in the syntax of external ip of your Riva Server:Port of your Riva Server
           # Example: 12.34.56.789:50050
